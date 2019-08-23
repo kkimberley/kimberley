0f8c1fa3dd1f9ecf79aecfb42ca47cb5319c6443
kim <kim@kimde-MacBook-Pro-3.local>
2019-08-23 12:09:49 +0800

    new feature: signup

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# user model
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, uniqueness: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, confirmation: true,
+                       length: { minimum: 8 }
+end
